38caea81be59d80c1be4939727780d430377a5f4
GeovanaRamos <geovannna.1998@gmail.com>
2019-09-10 13:27:01 -0300

    Added performance factor calculation

--- block_student_performance.php
+++ block_student_performance.php
@@ -1,10 +1,10 @@
 <?php
 class block_student_performance extends block_base {
-    
+
     public function init() {
         $this->title = get_string('student_performance', 'block_student_performance');
     }
-    
+
     public function get_content() {
         global $CFG;
         global $COURSE;
@@ -12,7 +12,7 @@ class block_student_performance extends block_base {
 
         require_once($CFG->dirroot . '/blocks/student_performance/locallib.php');
 
-        // Hide the block to non-logged in users and guests 
+        // Hide the block to non-logged in users and guests
         if ($this->content !== null || !isloggedin() || isguestuser()) {
           return $this->content;
         }
@@ -21,17 +21,16 @@ class block_student_performance extends block_base {
         $this->page->requires->js("/blocks/student_performance/js/gauge.min.js");
         $this->page->requires->js("/blocks/student_performance/js/gauge.js");
 
-        $gradeitems = block_student_performance_get_grade_items($COURSE->id);
-        $itemscompleted = block_student_performance_get_items_completed($COURSE->id, $USER->id);
+        $performancefactor = block_student_performance_get_performance_factor($COURSE->id, $USER->id);
 
         $this->content         =  new stdClass;
         $this->content->text   = html_writer::tag(
             'canvas',
-            '', 
-            array('id' => 'gauge', 'data-perf' => $itemscompleted*10/(float)$gradeitems)
+            '',
+            array('id' => 'gauge', 'data-perf' => $performancefactor)
         );
         $this->content->footer = '';
-     
+
         return $this->content;
     }
 
--- locallib.php
+++ locallib.php
@@ -1,9 +1,39 @@
 <?php
 
+function block_student_performance_get_performance_factor($courseid, $userid){
+    /*
+      Factor(F) is given by:
+
+      F =  AC * 10 / AT
+
+      AC = (Activities completed by student / Days of enrolment)
+      AT = (Total gradable activities / Course duration in days)
+
+    */
+
+    // Enrolment duration informations
+    $enrolinfo = block_student_performance_get_enrol_info($courseid, $userid);
+
+    // Gradable activities count
+    $itemscompleted = block_student_performance_get_items_completed($courseid, $userid);
+    $gradeitems = block_student_performance_get_grade_items($courseid);
+
+    // Variables for performance factor calculation
+    $itemsperday = $gradeitems / float(ceil($enrolinfo->enrolperiod / 86400));
+    $timeenrolled = time() - $enrolinfo->timestart;
+    $completedperday = $itemscompleted / float(ceil($timeenrolled / 86400));
+
+    // Performance factor formula
+    $factor = $completedperday * 10 / float($itemsperday);
+
+    return $factor;
+
+}
+
 function block_student_performance_get_grade_items($courseid){
     global $DB;
 
-    $sql = "SELECT COUNT(*) FROM {grade_items} 
+    $sql = "SELECT COUNT(*) FROM {grade_items}
             WHERE courseid=? AND itemtype!='course'";
 
     $count = $DB->count_records_sql($sql, [$courseid]);
@@ -15,12 +45,25 @@ function block_student_performance_get_items_completed($courseid, $userid){
     global $DB;
 
     $sql = "SELECT COUNT(*)
-            FROM {grade_items} i 
-            INNER JOIN {grade_grades} g ON i.id=g.itemid 
-            WHERE i.courseid=? AND g.userid=? 
+            FROM {grade_items} i
+            INNER JOIN {grade_grades} g ON i.id=g.itemid
+            WHERE i.courseid=? AND g.userid=?
             AND itemtype='mod' AND g.aggregationstatus='used'";
 
     $count = $DB->count_records_sql($sql, [$courseid, $userid]);
 
     return $count;
+}
+
+function block_student_performance_get_enrol_info($courseid, $userid){
+    global $DB;
+
+    $sql = "SELECT ue.timestart, ue.timeend, e.enrolperiod
+           FROM {user_enrolments} ue, {enrol} e
+           WHERE ue.userid=? AND ue.enrolid=e.id AND e.courseid=?";
+
+    $enrolinfo = $DB->get_record_sql($sql, [$userid, $courseid]);
+
+    return $enrolinfo;
+
 }
